0914a5d6f2f134f16f1c076332197ce432ea13f3
François <mockersf@gmail.com>
2022-07-06 12:27:19 +0200

    cli more helpful

--- jornet-cli/src/main.rs
+++ jornet-cli/src/main.rs
@@ -1,7 +1,38 @@
 use biscuit_auth::KeyPair;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate a new biscuit key
+    GenerateBiscuitKey {
+        /// Output as dhall format
+        #[clap(short, long, value_parser)]
+        dhall: bool,
+    },
+}
+
 fn main() {
-    let root = KeyPair::new();
-    let private_key = root.private();
-    println!("{}", base64::encode(private_key.to_bytes()));
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::GenerateBiscuitKey { dhall } => {
+            let root = KeyPair::new();
+            let private_key = root.private();
+            let output = if dhall {
+                format!("\"{}\"", base64::encode(private_key.to_bytes()))
+            } else {
+                format!("{}", base64::encode(private_key.to_bytes()))
+            };
+            println!("{}", output);
+        }
+    }
 }
