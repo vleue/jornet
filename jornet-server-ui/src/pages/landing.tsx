f5920febe4a3641a9b48df16e0edaf85d297daf0
François <mockersf@gmail.com>
2022-07-22 23:45:55 +0200

    include the wasm on landing page

--- jornet-server-ui/src/pages/landing.tsx
+++ jornet-server-ui/src/pages/landing.tsx
@@ -4,22 +4,40 @@ import { Col, Container, Row } from "react-bootstrap";
 class Landing extends Component {
     render() {
         return (
-            <Container fluid="lg">
-                <Row>
-                    <Col>&nbsp;</Col>
-                </Row>
-                <Row>
-                    <Col></Col>
-                    <Col xs={10}>
-                        <big>
-                            Jornet is the social game server made for game jams!
-                        </big>
-                    </Col>
-                    <Col></Col>
-                </Row>
-            </Container>
+            <>
+                <Container fluid="lg">
+                    <Row>
+                        <Col>&nbsp;</Col>
+                    </Row>
+                    <Row>
+                        <Col></Col>
+                        <Col xs={10}>
+                            <big>
+                                Jornet is the social game server made for game jams!
+                            </big>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                    <Row>
+                        <Col>&nbsp;</Col>
+                    </Row>
+                    <Row>
+                        <Col><a href="https://jornet.vleue.com/leaderboard/fb0bbe22-b047-494d-9519-1d36668fa5bc" target="_blank">View the example leaderboard</a></Col>
+                    </Row>
+                    <Row>
+                        <Col><canvas id="demo-leaderboard"></canvas></Col>
+                    </Row>
+                </Container>
+
+            </>
         );
     }
+    componentDidMount() {
+        const script = document.createElement('script');
+        script.type = "module";
+        script.textContent = "import init from './demo_leaderboard.js'; init();";
+        document.body.appendChild(script);
+    };
 }
 
 export default Landing;
