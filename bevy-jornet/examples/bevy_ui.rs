31e76a1cab7e342d701200a5f35a689a9ae5aba5
François <mockersf@gmail.com>
2022-07-22 22:56:49 +0200

    display something in example

--- bevy-jornet/examples/bevy_ui.rs
+++ bevy-jornet/examples/bevy_ui.rs
@@ -1,26 +1,57 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, winit::WinitSettings};
 use bevy_jornet::{JornetPlugin, Leaderboards};
 use uuid::Uuid;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(WinitSettings::desktop_app())
         .add_plugin(JornetPlugin)
-        .add_startup_system(test)
+        .add_startup_system(setup)
         .add_system(display_scores)
         .run();
 }
 
-fn test(mut leaderboards: ResMut<Leaderboards>) {
+fn setup(mut commands: Commands, mut leaderboards: ResMut<Leaderboards>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(NodeBundle {
+        style: Style {
+            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+            margin: UiRect::all(Val::Auto),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            flex_direction: FlexDirection::ColumnReverse,
+            ..default()
+        },
+        ..default()
+    });
     leaderboards.set_dashboard(Uuid::parse_str("fb0bbe22-b047-494d-9519-1d36668fa5bc").unwrap());
-    leaderboards.send_score(120.7);
     leaderboards.refresh_leaderboard();
+    // leaderboards.send_score(59.8);
 }
 
-fn display_scores(leaderboards: Res<Leaderboards>, mut done: Local<bool>) {
+fn display_scores(
+    leaderboards: Res<Leaderboards>,
+    mut done: Local<bool>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    root_ui: Query<Entity, (With<Node>, Without<Parent>)>,
+) {
     if !*done {
         if !leaderboards.get_leaderboard().is_empty() {
-            info!("{:?}", leaderboards.get_leaderboard());
+            let root_ui = root_ui.single();
+            for score in leaderboards.get_leaderboard() {
+                commands.entity(root_ui).with_children(|parent| {
+                    parent.spawn_bundle(TextBundle::from_section(
+                        format!("{} ", score.score),
+                        TextStyle {
+                            font: asset_server.load("FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.2, 0.3, 0.2),
+                        },
+                    ));
+                });
+            }
             *done = true;
         }
     }
