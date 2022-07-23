3a190227c75f9e0b12529c29eb9883fb2954fb9d
François <mockersf@gmail.com>
2022-07-23 20:38:02 +0200

    don't care about the response

--- bevy-jornet/src/http.rs
+++ bevy-jornet/src/http.rs
@@ -41,10 +41,8 @@ pub(crate) async fn post<T: Serialize>(url: &str, body: T) {
         let request = Request::new_with_str_and_init(&url, &opts).unwrap();
 
         let window = web_sys::window().unwrap();
-        let resp_value = JsFuture::from(window.fetch_with_request(&request))
+        JsFuture::from(window.fetch_with_request(&request))
             .await
             .unwrap();
-        let resp: Response = resp_value.dyn_into().unwrap();
-        JsFuture::from(resp.json().unwrap()).await.unwrap();
     }
 }
